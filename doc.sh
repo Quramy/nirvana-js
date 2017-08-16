dd796aee14be8c57e9e84989bb24325d2857f4ac
y-kurami <yosuke.kurami@gmail.com>
2017-08-16 18:38:43 +0900

    Add doc md tool

---
+++ doc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+declare projectDir=$(git rev-parse --show-toplevel)
+declare srcDir="${projectDir}/lib"
+declare docDir="${projectDir}/docs"
+# add build result
+function addDoc(){
+    declare filePath=$1;
+    declare fileName=$(basename ${filePath} "d.ts")
+    $(npm bin)/docco --blocks -t ${projectDir}/tools/d.ts-markdown.jst ${filePath} --output ${projectDir}/__obj/docs
+    mv ${projectDir}/__obj/docs/**${fileName}*.html ${projectDir}/docs/api/${fileName}md
+    echo "Create: ${docDir}/api/${fileName}md"
+}
+# npm run build
+# update
+addDoc "${srcDir}/renderer/client-fns.d.ts"
+# addDoc "${srcDir}/Dispatcher.d.ts"
+# addDoc "${srcDir}/DispatcherPayloadMeta.d.ts"
+# addDoc "${srcDir}/Store.d.ts"
+# addDoc "${srcDir}/UILayer/StoreGroup.d.ts"
+# addDoc "${srcDir}/UseCase.d.ts"
+# addDoc "${srcDir}/UseCaseContext.d.ts"
---
+++ templates/nirvana.conf.js
@@ -0,0 +1,7 @@
+module.exports = {
+  show: false,        // Whether to desplay browser windows.
+  watch: false,       // Watch script files and reload window when they are changed.
+  concurrency: 4,     // How many windows Nirvana launches in parallel.
+  capture-console: true,      // Whether to capture logging message in browser.
+  // customContextFile: "my-context.html", // HTML context file.
+};
