22922df6f661c93b8cc244e4ee206a215d54cacf
y-kurami <yosuke.kurami@gmail.com>
2017-08-16 18:28:18 +0900

    Init option

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -2,6 +2,8 @@ import * as path from "path";
 import * as fs from "fs";
 import { spawn } from "child_process";
 import * as yargs from "yargs";
+import * as chalk from "chalk";
+const cpf = require("cp-file");
 
 function bootstrap(electronOpt: any) {
   const electronPath: string = (require("electron") as any);
@@ -17,11 +19,12 @@ function bootstrap(electronOpt: any) {
 
 function main() {
   yargs
-    .usage("Usage: $0 [options]")
+    .usage("Usage: $0 [options] script_file ... script_file")
     .options("h", { alias: "help" })
     .options("c", { alias: "config", desc: "Configuration file path.", string: true })
     .options("b", { alias: "base-path", desc: "The root path location to be used to resolve from.", string: true })
-    .options("w", { alias: "watch", desc: "Watch mode", boolean: true, default: false })
+    .options("w", { alias: "watch", desc: "Watch script files and reload window when they are changed.", boolean: true, default: false })
+    .options("init", { desc: "Generate configuration file.", boolean: true })
     .options("show", { desc: "Whether to desplay browser windows." , boolean: true, default: false })
     .options("concurrency", { desc: "How many windows Nirvana launches in parallel." , number: true, default: 4 })
     .options("capture-console", { desc: "Whether to capture logging message in browser.." , boolean: true, default: true })
@@ -30,6 +33,15 @@ function main() {
   const configFileName = yargs.argv.config || "nirvana.conf.js";
   const target = yargs.argv._;
   let hasConf =  fs.existsSync(configFileName);
+  if (yargs.argv.init) {
+    const tmpl = path.resolve(__dirname, "../../templates/nirvana.conf.js");
+    const dist = "./nirvana.conf.js";
+    cpf(tmpl, dist).then(() => {
+      console.log(`Successfully created a configuration file to ${chalk.magenta("nirvana.conf.js")}`);
+      process.exit(0);
+    });
+    return;
+  }
   if (hasConf) {
     bootstrap({
       target,
